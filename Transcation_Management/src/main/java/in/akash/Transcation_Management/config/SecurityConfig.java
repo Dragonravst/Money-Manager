125c9488d42b71fed81f8029f45091228138a43e
Dragonravst <ravst.rb@gmail.com>
2025-10-08 17:41:27 +0530

    "Deployed the backend project"

--- Transcation_Management/src/main/java/in/akash/Transcation_Management/config/SecurityConfig.java
+++ Transcation_Management/src/main/java/in/akash/Transcation_Management/config/SecurityConfig.java
@@ -34,12 +34,16 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth->auth.requestMatchers("/status","/health","/register","/activate","/login").permitAll()
-                .anyRequest().authenticated())
-                .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/status", "/health", "/api/v1.0/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+
         return httpSecurity.build();
     }
+
 
     @Bean
     public PasswordEncoder passwordEncoder() {
